f594245a41de17952e10cb325c24adf640fb2c36
SookieYuan <sookie19931009@gmail.com>
2021-04-15 17:50:03 -0400

    Add files via upload

---
+++ JoinUs/app.js
@@ -0,0 +1,53 @@
+var express = require('express');
+var mysql = require('mysql');
+var bodyParser  = require("body-parser");
+var app = express();
+ 
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + "/public"));
+
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  database : 'join_us'
+});
+
+app.get("/", function(req, res){
+    //find count of users in DB
+    //response with that count
+    var q = "SELECT COUNT(*) AS count FROM users";
+    connection.query(q, function(err, results){
+        if(err) throw err;
+        var count = results[0].count; 
+        res.render("home", {count: count});
+    });
+});
+
+app.post("/register", function(req, res){
+    var person = {
+        email: req.body.email
+    };
+    connection.query('INSERT INTO users SET ?', person, function(err, result) {
+        if (err) throw err;
+        res.redirect("/");
+    });
+});
+
+
+// //Add a /joke route
+// app.get("/joke", function(req, res){
+//  var joke = "<strong> What do you call a dog that does magic tricks? </strong> <em>A labracadabrador</em>.";
+//  res.send(joke);
+// });
+
+// //Add a /random_num route
+// app.get("/random_num", function(req, res){
+//  var num = Math.floor((Math.random() * 10) + 1);
+//  res.send("Your lucky number is " + num);
+// });
+
+app.listen(3000, function () {
+ console.log('App listening on port 3000!');
+});
+
---
+++ schema_copy1.sql
@@ -0,0 +1,13 @@
+CREATE TABLE users (
+    email VARCHAR(255) PRIMARY KEY,
+    created_at TIMESTAMP DEFAULT NOW()
+);
+
+INSERT INTO users (email) VALUES
+('Katie34@yahoo.com'), ('Tunde@gmail.com');
+
+var q = 'SELECT * FROM users ';
+connection.query(q, function (error, results, fields) {
+  if (error) throw error;
+  console.log(results);
+});
